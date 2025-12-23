9d6c84e8674d920cef2ffb9b1fadffd32c44223d
Alan Raphael <alanraphael7@gmail.com>
2025-12-23 14:34:18 -0300

    fix debusg

--- src/routes/debug.routes.js
+++ src/routes/debug.routes.js
@@ -3,6 +3,9 @@ const DebugShopeeController = require("../controllers/DebugShopeeController");
 
 const router = express.Router();
 
+router.get("/debug/ping", (req, res) =>
+  res.json({ status: "ok", debug: true })
+);
 router.get("/debug/shops/:shopId/orders/list", DebugShopeeController.orderList);
 router.get(
   "/debug/shops/:shopId/orders/detail",
